d64b4267df23b7b008593565863e80a40d1c02eb
Vipul <vmbansal2001@gmail.com>
2020-07-01 11:59:54 +0530

    Queues

---
+++ PracticeQuestions/src/DynamicQueue/DynamicQueueClient.java
@@ -0,0 +1,42 @@
+package DynamicQueue;
+
+import QueueUsingArrays.QueueUsingArrays;
+
+public class DynamicQueueClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		QueueUsingArrays queue = new DynamicQueue(5);
+
+		for (int i = 1; i <= 5; i++) {
+			queue.enqueue(i*10);
+			queue.display();
+		}
+		
+		queue.enqueue(90);
+		System.out.println(queue.size());
+		
+		System.out.println(queue.dequeue());
+		System.out.println(queue.dequeue());
+		
+		queue.display();
+		
+		queue.isEmpty();
+		queue.enqueue(60);
+		queue.enqueue(70);
+		queue.display();
+		System.out.println(queue.front());
+		
+		while (!queue.isEmpty())
+		{
+			System.out.println(queue.dequeue());
+		}
+		
+//		queue.dequeue();
+		
+		System.out.println("BYE");
+
+	}
+
+}
